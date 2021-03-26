ee38ddf624a8e0099e4aea313e1731e89b86c20b
Administrator <Administrator@host.docker.internal>
2021-03-26 15:13:49 +0900

    0129Test

---
+++ src/AntTest/Test1.java
@@ -0,0 +1,8 @@
+package AntTest;
+
+public class Test1 {
+	public static void main(String[] args) {
+		
+		System.out.println("Test1.java 입니다.");
+	}
+}
---
+++ src/AntTest/Test2.java
@@ -0,0 +1,8 @@
+package AntTest;
+
+public class Test2 {
+	public static void main(String[] args) {
+		
+		System.out.println("Test2.java 입니다.");
+	}
+}
